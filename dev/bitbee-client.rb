d10b9d1355b600ea7bd2b884155c51727e229d2f
Piotr Gaertig <github@gaertig.pl>
2023-12-03 22:21:15 +0100

    Update Bitbee Client dev tool

--- dev/bitbee-client.rb
+++ dev/bitbee-client.rb
@@ -1,12 +1,19 @@
-#!/usr/bin/env jruby
+#!/usr/bin/env -S jruby -J--enable-preview -J-Djdk.httpclient.HttpClient.log=errors,requests,headers,content
+# -J-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=1044
 require 'pry'
 require 'json'
+logback_path = "#{__dir__}/dev-logback.xml"
+java.lang.System.setProperty("logback.configurationFile", logback_path)
+java.lang.System.setProperty("slf4j.provider","ch.qos.logback.classic.spi.LogbackServiceProvider")
 $LOAD_PATH.unshift(__dir__) unless $LOAD_PATH.include?(__dir__)
-app_fat_jar = Dir["../target/mercury*.jar"].first || raise("This script needs Spring Boot fat JAR based app available in target/")
-lib_jars = Dir["./#{app_fat_jar}!/BOOT-INF/lib/*.jar"]
-raise("No Spring Boot libraries found in #{app_fat_jar}") if lib_jars.empty?
-lib_jars.sort.each {|jar| require jar}
-$CLASSPATH << '../target/classes'
+begin
+  $CLASSPATH << File.read("#{__dir__}/../target/dev-classpath.txt").split(":")
+rescue => e
+  $stderr.puts e
+  $stderr.puts "Run at least `mvn compile` to generate classes"
+  exit(1)
+end
+$CLASSPATH << "#{__dir__}/../target/classes"
 
 module Kernel
   def pl
@@ -19,8 +26,10 @@ if ARGV.empty?
   exit 1
 end
 
+logger = Java::OrgSlf4j::LoggerFactory.getLogger java.util.Date.java_class
+logger.info("Starting bitbee-client.rb")
 config = pl.amitec.mercury.util.StructUtils.propertiesFileToMap(ARGV[0])
-client = pl.amitec.mercury.providers.bitbee.BitbeeClient.new(config)
+client = pl.amitec.mercury.clients.bitbee.BitbeeClient.new(config)
 client.session {
   binding.pry
 }
