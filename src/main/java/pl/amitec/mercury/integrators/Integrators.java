e265faf40e7299b8cb9f9e1ff11ebf9a4b5bde15
Piotr Gaertig <github@gaertig.pl>
2025-09-22 20:27:23 +0200

    ** Removal of RedBay integrator **

--- src/main/java/pl/amitec/mercury/integrators/Integrators.java
+++ src/main/java/pl/amitec/mercury/integrators/Integrators.java
@@ -2,13 +2,11 @@ package pl.amitec.mercury.integrators;
 
 import pl.amitec.mercury.Integrator;
 import pl.amitec.mercury.integrators.polsoft.PolsoftToBitbeeIntegrator;
-import pl.amitec.mercury.integrators.redbay.RedbayToBitbeeIntegrator;
 
 import java.util.Map;
 
 public interface Integrators {
     Map<String, Class<? extends Integrator>> list = Map.of(
-        PolsoftToBitbeeIntegrator.NAME, PolsoftToBitbeeIntegrator.class,
-        RedbayToBitbeeIntegrator.NAME, RedbayToBitbeeIntegrator.class
+        PolsoftToBitbeeIntegrator.NAME, PolsoftToBitbeeIntegrator.class
     );
 }
